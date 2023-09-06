62c44e3bfa1a00c8919a0c2f136e2095c5da909e
Nataliia Solomchak <natalia2001727@gmail.com>
2023-09-06 14:58:17 +0100

    Add questions array with oblects to javascript file

---
+++ assets/js/questions.js
@@ -0,0 +1,107 @@
+//Questions for all quizzes
+let questions = [
+    {
+        numb: 1,
+        question: "Which HTML tag is used to define an unordered list?",
+        answers: ["&#60;ol&#62;", "&#60;ul&#62;", "&#60;list&#62;", "&#60;dl&#62;"],
+        correctAnswer: "<ul>"
+    },
+
+    {
+        numb: 2,
+        question: "What is the correct HTML element for inserting an image?",
+        answers: ["&#60;img&#62;", "&#60;picture&#62;", "&#60;image&#62;", "&#60src&#62;"],
+        correctAnswer: "<img>"
+    },
+
+    {
+        numb: 3,
+        question: "Which HTML element represents a paragraph of text?",
+        answers: ["&#60prg&#62;", "&#60pg&#62;", "&#60p&#62;", "&#60{p}&#62;"],
+        correctAnswer: "<p>"
+    },
+
+    {
+        numb: 4,
+        question: "What is the correct HTML for italicizing text?",
+        answers: ["&#60italic&#62;text&#60/italic&#62;", "&#60em&#62;text&#60/em&#62;", "&#60i&#62;text&#60/i&#62;", "&#60italics&#62;text&#60/italics&#62;"],
+        correctAnswer: "<i>text</i>"
+    },
+
+    {
+        numb: 5,
+        question: "Which HTML element is used to group related legend and fieldset elements?",
+        answers: ["&#60i&#62;label&#60/i&#62;", "&#60i&#62;details&#60/i&#62;", "&#60i&#62;optgroup&#60/i&#62;", "&#60i&#62;form&#60/i&#62;"],
+        correctAnswer: "<i>form</i>"
+    },
+
+    {
+        numb: 6,
+        question: "Which CSS property controls the text color?",
+        answers: ["text-color", "font-color", "color", "txt-color"],
+        correctAnswer: "color"
+    },
+
+    {
+        numb: 7,
+        question: "Which is the CSS syntax for a comment?",
+        answers: ["/* comment */", "// comment", "' comment '", "! comment !"],
+        correctAnswer: "/* comment */"
+    },
+
+    {
+        numb: 8,
+        question: "Which CSS property makes text bold?",
+        answers: ["text-bold: true;", "font-weight: bold;", "font-size: bold;", "style: bold;"],
+        correctAnswer: "font-weight: bold;"
+    },
+
+    {
+        numb: 9,
+        question: "How do you select all paragraph elements?",
+        answers: ["paragraph { }", "p { }", "all &#60&#62;p&#60&#62; { }", "#p { }"],
+        correctAnswer: "p { }"
+    },
+
+    {
+        numb: 10,
+        question: "Which of the following is the correct syntax to display the hyperlinks without any underline?",
+        answers: ["a {decoration : no-underline;}", "a {text-decoration : underline;}", "a {text-decoration : none;}", "None"],
+        correctAnswer: "a {text-decoration : none;}"
+    },
+
+    {
+        numb: 11,
+        question: "Which of the following is not a primitive data type in JavaScript?",
+        answers: ["number", "string", "boolean", "object"],
+        correctAnswer: "object"
+    },
+
+    {
+        numb: 12,
+        question: "What does the “NaN” value represent in JavaScript?",
+        answers: ["not a number", "null value", "undefined value", "boolean value"],
+        correctAnswer: "not a number"
+    },
+
+    {
+        numb: 13,
+        question: "What is the correct way to declare a variable in JavaScript?",
+        answers: ["var x = 5;", "variable x = 5;", "x = 5;", "let x = 5;"],
+        correctAnswer: "let x = 5;"
+    },
+
+    {
+        numb: 14,
+        question: "What is the correct syntax for a “for” loop in JavaScript?",
+        answers: ["for (var i = 0; i < 5; i++)", "for (i = 0; i < 5; i++)", "for (var i = 5; i > 0; i–)", "for (i = 5; i > 0; i–)"],
+        correctAnswer: "for (var i = 0; i < 5; i++)"
+    },
+
+    {
+        numb: 15,
+        question: "Which of the following is not a loop in JavaScript?",
+        answers: ["for", "while", "do…while", "next"],
+        correctAnswer: "next"
+    }
+]
