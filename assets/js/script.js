044d20dad6706d5725729a3afcc27507515ef862
Nataliia Solomchak <natalia2001727@gmail.com>
2023-09-06 16:11:33 +0100

    Add comments to showQuizQuestions function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,19 +18,31 @@ startQuiz();
 const answersList = document.querySelector('.answers-list')
 
 function showQuizQuestions(index) {
+    /**
+     * Gets the .question-heading element
+     * Sets its text to the current question number and text from the questions array
+     *  */ 
     const questionHeading = document.querySelector('.question-heading')
     questionHeading.textContent = `${questions[index].numb}. ${questions[index].question}`
-
+    
+    /**
+     * Builds a string containing HTML for each answer choice 
+     * By inserting the answers from the array into <li> tags
+     */
     const answerEach = `<li class="answer">${questions[index].answers[0]}</li>
     <li class="answer">${questions[index].answers[1]}</li>
     <li class="answer">${questions[index].answers[2]}</li>
     <li class="answer">${questions[index].answers[3]}</li>`
 
+    /**
+     * Sets the innerHTML of the answersList element to the answerEch string
+     * Displays the answers on the page
+     */
     answersList.innerHTML = answerEach
 
     /**
      * The code for this part of the function is taken from Codehal's Quiz Website Youtube video
-     * Loops through all elements with class "answer" and attaches aclick handler to each one
+     * Loops through all elements with class "answer" and attaches a click handler to each one
      */
     const answer = document.querySelectorAll('.answer');
     for(let i = 0; i < answer.length; i++) {
